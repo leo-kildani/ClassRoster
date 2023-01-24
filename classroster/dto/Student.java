f192891d2467f58f34a994b25fdce5b9d0361cf0
Leonardo Kildani <leonardo.kildani@sjsu.edu>
2023-01-24 12:33:57 -0800

    add Student dto

--- classroster/dto/Student.java
+++ classroster/dto/Student.java
@@ -1,6 +1,42 @@
 package classroster.dto;
 
+import java.util.Random;
+
 public class Student {
 	private String firstName, lastName;
+	final String studentID;
+	private String cohort;
 	
+	public Student(String id) {
+		studentID = id;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCohort() {
+		return cohort;
+	}
+
+	public void setCohort(String cohort) {
+		this.cohort = cohort;
+	}
+
+	public String getStudentID() {
+		return studentID;
+	}
+
 }
