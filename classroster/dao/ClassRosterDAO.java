28600ae7886a474663edea3add1f0fa07832866f
Leonardo <leonardo.kildani@sjsu.edu>
2023-01-25 13:17:35 -0800

    spell check in javadoc

--- classroster/dao/ClassRosterDAO.java
+++ classroster/dao/ClassRosterDAO.java
@@ -7,18 +7,18 @@ import classroster.dto.Student;
 public interface ClassRosterDAO {
 	
 	/**
-	 * Adds athe given Student to the roster and associates it with the given student id.
+	 * Adds the given Student to the roster and associates it with the given student id.
 	 * If there is already a student with the given student id it will return that student object.
 	 * Otherwise it will return null.
 	 * 
-	 * @param studentID id with which student is to be associated with
-	 * @param Student student to be added to the roster
-	 * @return id if it exists, owtherwise null
+	 * @paramstudentID id with which student is to be associated with
+	 * @paramStudent student to be added to the roster
+	 * @return id if it exists, otherwise null
 	 */
 	Student addStudent(String studentID, Student student);
 
 	/**
-	 * Retusn a List of all students in the roster
+	 * Returns a List of all students in the roster
 	 * 
 	 * @return List containing all the students
 	 */
@@ -28,7 +28,7 @@ public interface ClassRosterDAO {
 	 * Returns the student associated with the given id.
 	 * Return null if no such student exists
 	 * 
-	 * @param studentID id to be looked for
+	 * @paramstudentID id to be looked for
 	 * @return student associated with id, null otherwise
 	 */
 	Student getStudent(String studentID);
@@ -37,7 +37,7 @@ public interface ClassRosterDAO {
 	 * Removes student from roster associated with the given id.
 	 * Return the student that has been removed or null if no student was found
 	 * 
-	 * @param studentID id of student to be removed
+	 * @paramstudentID id of student to be removed
 	 * @return Student if student found and removed, null otherwise
 	 */
 	Student removeStudent(String studentID);
